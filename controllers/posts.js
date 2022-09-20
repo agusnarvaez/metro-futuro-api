c9e48fc9a306008d45eea8fb7390a9565a546dc4
agusnarvaez <agus.narvaez@outlook.com>
2022-09-20 15:53:01 -0300

    Agregué el método getPosts que devuelve todos los arrays del modelo

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,15 @@
+import PostMessage from "../models/postMessage.js"
+
 export const getPosts = (req,res)=>{
-    res.send('This works')
+    
+    PostMessage.find()
+        .then((postMessages)=>{
+            res.status(200).json(postMessages)
+        })
+        .catch((error)=>res.status(404).json({message:error.message}))
+    /* res.send('This works') */
+}
+
+export const createPost = (req,res)=>{
+    res.send("Post Creation")
 }
--- models/postMessage.js
+++ models/postMessage.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const postSchema = mongoose.Schema({
-    id: number,
+    id: Number,
     title: String,
     cover: String,
     shortDescription: String,
