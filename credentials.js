324d512a0fd22cfb654ea787d089201fbad7add0
agusnarvaez <agus.narvaez@outlook.com>
2022-09-20 14:07:12 -0300

    Configuración inicial MongoDB | Mongoose

---
+++ credentials.js
@@ -0,0 +1,6 @@
+
+
+const credentials ={
+    mongoDB:"mongodb+srv://metro-futuro:rF8QGIwNftmqBzmq@cluster0.5itxahn.mongodb.net/?retryWrites=true&w=majority"    
+}
+export default credentials;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import credentials from "./credentials.js"
 //* Inicializamos la función express como app
 const app = express()
 
@@ -12,4 +12,18 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 
 //* Is used to specitfy what domains can access the API
 app.use(cors())
+
+//* URL de la base de datos
+const CONNECTION_URL = credentials.mongoDB
+//* Puerto en el que se ejecutará el servidor
+const PORT = process.env.port || 5000
+
+
+//* Importamos las rutas a mongoose y las usamos
+mongoose.connect(CONNECTION_URL)
+    .then(()=>{
+        //* Iniciamos el servidor una vez conectado a la base de datos
+        app.listen(PORT,()=> console.log(`Server running on port http://localhost:${PORT}`))}
+        )
+    .catch((error)=>console.log(error.message))
 
